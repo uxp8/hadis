f1b66b40144253b0b26528f1a70e83f9c4e7fe9e
黑海洋 <tzsm11@qq.com>
2017-04-16 15:57:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,10 @@
-<?php
 
-/*
- * This file is part of Flarum.
- *
- * (c) Toby Zerner <toby.zerner@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
 
-require 'vendor/autoload.php';
 
-$server = new Flarum\Forum\Server(__DIR__);
+This file is part of Flarum.
 
-$server->listen();
+ (c) Toby Zerner <toby.zerner@gmail.com>
+
+ For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+
